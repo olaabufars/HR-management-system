ae846af18565c09c2525a67370d2011d26ffe623
olaabufars <olaabufars@gmail.com>
2022-01-25 13:04:17 +0200

    hr

--- js/l.js
+++ js/l.js
@@ -8,41 +8,45 @@ function Employee(EmployeeID,FullName,Department, level ){
     this.FullName = FullName;
     this.Department = Department;
     this.level = level;
+    this.Salary = 0;
    
     
 }
 const employee1 = new Employee(1000,"Ghazi Samer","Administration","Senior");
 
 const employee2 = new Employee(1001,"Lana Ali","Finance","Senior");
+const employee3 = new Employee(1002,"Tamara Ayoub","Marketing","Senior");
+const employee4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior");
+const employee4 = new Employee(1004,"Omar Zaid","Development","Senior");
+const employee4 = new Employee(1005,"Rana Saleh","Development","Junior");
+const employee4 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
 
 
 
-var salarytax;
+
 Employee.prototype.CalculatSalary= function(){
 if(this.level === "Senior"){
     min = 1500;
     max = 2000;
-    this.Salary =Math.random() * (max - min) + min;
-    salarytax =(this.Salary*7.5);
+    this.Salary =(Math.random() * (max - min) + min)*7.5;
 }
 else if(this.level === "Mid-Senior"){
     min = 1000;
     max = 1500;
-    this.Salary =Math.random() * (max - min) + min;
-    salarytax =(this.Salary*7.5);
+    this.Salary =(Math.random() * (max - min) + min)*7.5;
 }
 else{
     min = 500;
     max = 1000;
-    this.Salary =Math.random() * (max - min) + min;
-    salarytax =(this.Salary*7.5);
+    this.Salary =(Math.random() * (max - min) + min)*7.5;
 
 }
 }
 //var gitReturn = employee1.CalculatSalary();
 
 Employee.prototype.render = function(){
-    document.write(`<h1>${this.EmployeeID}  ${this.FullName}     ${this.Department}  ${this.level}    ${this.CalculatSalary()} </h1>`)
+    document.write(`<h1>${this.EmployeeID}  ${this.FullName}     ${this.Department}  ${this.level}    ${this.Salary} </h1>`)
 }
+employee1.CalculatSalary();
 employee1.render();
 //employee2.render();
